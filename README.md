da9e15e2253444a8ac436231ee5ecb32b7ae0360
Javier Hernández <jhernandez@emergya.com>
2015-08-17 20:27:59 +0200

    Added initial system-wide integration with gsettings and xdg

--- README.md
+++ README.md
@@ -7,4 +7,18 @@ You can build and installing it by running:
 
 	./autogen.sh && make && make install
 
+## gsettings-integration branch
+
+This branch is a prototype of an integration between the GPII and the GNOME shell extension for the GPII.
+It includes:
+
+* An xdg-autostart file, located in the  _data_ folder
+* A system-wide gsettings schema, located in the _data_ folder 
+
+Currently, the extension:
+
+* Tracks the currently logged-in user by polling the flowManager
+* If there's a keyed-in user, the token is shown by the extension and it is possible to log the current user out of the GPII
+* The GPII can be start by changing a gsettings key (see _data_ folder)
+* There's a button to open the PMT
 
--- extension.js
+++ extension.js
@@ -1,5 +1,8 @@
 const Gio = imports.gi.Gio;
+const GLib = imports.gi.GLib;
+const Soup = imports.gi.Soup;
 const St = imports.gi.St;
+const Mainloop = imports.mainloop;
 const Lang = imports.lang;
 const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
@@ -31,7 +34,9 @@ const PopupImageMenuItem = new Lang.Class({
     }
 });
 
-const DEBUG = false;
+// Common debug and initialisation utilities and variables
+//
+const DEBUG = true;
 
 function debug(message)
 {
@@ -53,6 +58,9 @@ GPIIExtension.prototype = {
         PanelMenu.Button.prototype._init.call(this, 0);
         this.extensionMeta = extensionMeta;
 
+        // Create a Soup session
+        //session = Soup.Session.new();
+
         // Set-up Icon
         //
         iconPic = Gio.icon_new_for_string(extensionMeta.path +
@@ -70,52 +78,65 @@ GPIIExtension.prototype = {
 
         // Current User
         //
-        let loggedInLabel = new PopupImageMenuItem('No user has logged in yet',
-                                          'dialog-warning',
-                                          { reactive: false }
+        this.loggedInLabel = new PopupImageMenuItem(
+            'No user has logged in yet',
+            'dialog-warning',
+            { reactive: false }
         );
 
-        loggedInLabel.actor.set_style("font-style: italic; min-width: 250px;");
-        this.menu.addMenuItem(loggedInLabel);
+        this.loggedInLabel.actor.set_style("font-style: italic; min-width: 250px;");
+        this.menu.addMenuItem(this.loggedInLabel);
 
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
-        // GPII On/Off
+        // GPII On/Off toggle
         //
-        let gpiiStatus = new PopupMenu.PopupSwitchMenuItem("GPII Status", false);
+        let gpiiStatus = this._buildGpiiToggle("GPII Status", "net.gpii", "gpii-enabled"); 
         this.menu.addMenuItem(gpiiStatus);
 
-        // User Listeners SubMenu
-        //
-        let userListeners =  new PopupMenu.PopupSubMenuMenuItem("Authentication methods");
+        //// User Listeners SubMenu
+        ////
+        //let userListeners =  new PopupMenu.PopupSubMenuMenuItem("Authentication methods");
 
-        let usbListener = new PopupMenu.PopupSwitchMenuItem("USB Drive", false);
-        userListeners.menu.addMenuItem(usbListener);
+        //let usbListener = new PopupMenu.PopupSwitchMenuItem("USB Drive", false);
+        //userListeners.menu.addMenuItem(usbListener);
 
-        let nfcListener = new PopupMenu.PopupSwitchMenuItem("NFC Tag", false);
-        userListeners.menu.addMenuItem(nfcListener);
+        //let nfcListener = new PopupMenu.PopupSwitchMenuItem("NFC Tag", false);
+        //userListeners.menu.addMenuItem(nfcListener);
 
-        this.menu.addMenuItem(userListeners);
+        //this.menu.addMenuItem(userListeners);
 
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+        //this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
         // PMT/PCP Launchers
         //
-        let pmt = new PopupMenu.PopupMenuItem('Start using the GPII');
+        let pmt = new PopupMenu.PopupMenuItem('Preferences Management Tool');
+        pmt.connect('activate', Lang.bind(this, this._onPMTActivate));
         this.menu.addMenuItem(pmt);
 
-        let pcp = new PopupMenu.PopupMenuItem('Adjust your current settings');
-        pcp.actor.set_reactive(false);
-        this.menu.addMenuItem(pcp);
+        //let pcp = new PopupMenu.PopupMenuItem('Adjust your current settings');
+        //pcp.actor.set_reactive(false);
+        //this.menu.addMenuItem(pcp);
+
+        this.logout = new PopupMenu.PopupMenuItem('Log Out');
+        this.logout.connect('activate', Lang.bind(this, this._onLogoutActivate));
+        this.logout.actor.hide();
+        this.menu.addMenuItem(this.logout);
+
 
         this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
 
         // About GPII
         //
         let about = new PopupMenu.PopupMenuItem('About the GPII');
+        about.connect('activate', Lang.bind(this, this._onAboutGpiiActivate));
         this.menu.addMenuItem(about);
 
         this.menu.actor.set_style("min-width: 200px;");
+
+        // Check if a user is already logged in every 2.5 secs
+        //
+        Mainloop.timeout_add(2500, Lang.bind(this, this._checkLoggedInUsers));
     },
 
     enable: function() {
@@ -128,6 +149,106 @@ GPIIExtension.prototype = {
         this.__remove();
     },
 
+    _onPMTActivate: function () {
+        try {
+            var cmd = "firefox http://localhost:5559/demos/prefsEditor/index.html";
+            GLib.spawn_command_line_async(cmd, null);
+        } catch (e) {
+            throw e;
+        }
+    },
+
+    _onAboutGpiiActivate: function () {
+        try {
+            var cmd = "firefox http://gpii.net"
+            GLib.spawn_command_line_async(cmd, null);
+        } catch (e) {
+            throw e;
+        }
+    },
+
+    _onLogoutActivate: function () {
+        session = Soup.Session.new();
+        msg = Soup.Message.new("GET", "http://localhost:8081/user/" + this.__loggedInUserToken + "/logout");
+        session.queue_message(msg, Lang.bind(this, function (a, b) {
+            debug("Logged out " + this.__loggedInUserToken);
+        }));
+    },
+
+    _buildItemExtended: function(string, initial_value, writable, on_set) {
+        let widget = new PopupMenu.PopupSwitchMenuItem(string, initial_value);
+        if (!writable)
+            widget.actor.reactive = false;
+        else
+            widget.connect('toggled', function(item) {
+                on_set(item.state);
+            });
+        return widget;
+    },
+
+    _buildGpiiToggle: function(string, schema, key) {
+        let settings = new Gio.Settings({ schema: schema });
+        let widget = this._buildItemExtended(string,
+            settings.get_boolean(key),
+            settings.is_writable(key),
+            function(enabled) {
+                return settings.set_boolean(key, enabled);
+            });
+        settings.connect('changed::'+key, Lang.bind(this, function() {
+            widget.setToggleState(settings.get_boolean(key));
+            if (settings.get_boolean(key) === false) {
+                try {
+                    let [res, pid, in_fd, out_fd, err_fd] = GLib.spawn_async_with_pipes(null,
+                        ['/usr/bin/pgrep', '--full', 'node\ gpii.js'],
+                        null,
+                        0,
+                        null);
+
+                    let out_reader = new Gio.DataInputStream({
+                        base_stream: new Gio.UnixInputStream({'fd': out_fd})
+                    });
+
+                    let [pidId, size] = out_reader.read_line(null);
+                    GLib.spawn_command_line_async('kill -9 ' + pidId, null);
+                } catch (e) {
+                    throw e;
+                }
+            }
+        }));
+        return widget;
+    },
+
+    _checkLoggedInUsers: function () {
+        session = Soup.Session.new();
+        msg = Soup.Message.new("GET", "http://localhost:8081/userToken");
+        session.queue_message(msg, Lang.bind(this, this._getUserTokenCb));
+        return true;
+    },
+
+    _getUserTokenCb: function (session, msg, data) {
+        var isJSON = msg.response_body.data.charAt(0) === "{" ? true: false;
+
+        if (isJSON) {
+            var res = JSON.parse(msg.response_body.data);
+            if (res.isError) {
+                this.__loggedInUserToken = null;
+                this.logout.actor.hide();
+                this.loggedInLabel.label.set_text("No user has logged in yet");
+                this.loggedInLabel.actor.set_style("font-style: italic; min-width: 250px;");
+                this.loggedInLabel.setIcon("dialog-warning");
+            }
+        } else {
+            var userToken = eval(msg.response_body.data);
+            this.__loggedInUserToken = userToken;
+            this.logout.actor.show();
+            if(Object.prototype.toString.call(userToken) === '[object Array]') {
+                this.loggedInLabel.label.set_text("Current user: " + userToken);
+                this.loggedInLabel.actor.set_style("font-style: normal; font-weight: bold; min-width: 250px;");
+                this.loggedInLabel.setIcon("avatar-default");
+            }
+        }
+    },
+
     __add: function() {
         Main.panel._addToPanelBox('gpii', this, 0, Main.panel._rightBox);
         Main.panel.menuManager.addMenu(this.menu);
@@ -136,7 +257,9 @@ GPIIExtension.prototype = {
     __remove: function() {
         Main.panel._rightBox.remove_actor(this.container);
         Main.panel.menuManager.removeMenu(this.menu)
-    }
+    },
+
+    __loggedInUserToken: null
 }
 
 function init(extensionMeta) {
